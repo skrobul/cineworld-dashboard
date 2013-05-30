e8b9909d674b5f184e79738bd14041b96df35efb
Marek Skrobacki <marek.skrobacki@rackspace.co.uk>
2013-05-31 00:22:01 +0100

    fix bug in overwriting performances

--- lib/cinecheck.rb
+++ lib/cinecheck.rb
@@ -18,7 +18,7 @@ class CineChecker
 	def films_in_next_minutes(mins=60, interesting_cinemas=DEFAULT_INTERESTING_CINEMAS)
 		today = DateTime.now.strftime("%Y%m%d")
 		out = {}
-		seconds_to_midnight = 60 * 60 * 24 - DateTime.now.seconds_since_midnight
+		seconds_to_midnight = 60 * 60 * 24 - DateTime.now.in_time_zone("Europe/London").seconds_since_midnight
 		interesting_cinemas.each do |cinema|
 			begin
 				csymbol = Rails.cache.fetch("cached_cinema_#{cinema}", :expires_in => 100.days) do
@@ -36,7 +36,7 @@ class CineChecker
 			films.each do |film|
 				# from 'films' we need to resolve actual performances based on the EDI
 				edi = film['edi']
-				performances = Rails.cache.fetch("cached_performances_#{cinema}_#{film}", :expires_in => seconds_to_midnight) do
+				performances = Rails.cache.fetch("cached_performances_#{cinema}_#{edi}", :expires_in => seconds_to_midnight) do
 					@c.performances({ :cinema => cinema, :film => edi, :date => today})['performances']
 				end
 				performances.each do |perf|
@@ -44,7 +44,7 @@ class CineChecker
 					perfdate = DateTime.strptime(perf['time'], "%H:%M")
 					perfdate -= 1.hour if london_time.dst?
 					if perfdate > (london_time - mins.to_i.minutes)
-						out[csymbol][film['title']] = Array.new unless out.has_key?(film['title'])
+						out[csymbol][film['title']] = Array.new unless out[csymbol].has_key?(film['title'])
 						out[csymbol][film['title']] << perf['time']
 					end
 				end
