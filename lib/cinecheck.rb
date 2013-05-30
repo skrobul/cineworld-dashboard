d4fc805ec0504787c48a105ffcee1ccdcd1b8fc9
Marek Skrobacki <marek.skrobacki@rackspace.co.uk>
2013-05-30 22:47:23 +0100

    caching of the expensive/long queries

--- lib/cinecheck.rb
+++ lib/cinecheck.rb
@@ -18,20 +18,27 @@ class CineChecker
 	def films_in_next_minutes(mins=60, interesting_cinemas=DEFAULT_INTERESTING_CINEMAS)
 		today = DateTime.now.strftime("%Y%m%d")
 		out = {}
-
+		seconds_to_midnight = 60 * 60 * 24 - DateTime.now.seconds_since_midnight
 		interesting_cinemas.each do |cinema|
 			begin
-				csymbol = @c.cinemas({ :cinema => cinema })['cinemas'][0]['name']
+				csymbol = Rails.cache.fetch("cached_cinemas", :expires_in => 100.days) do
+					@c.cinemas({ :cinema => cinema })['cinemas'][0]['name']
+				end
 			rescue NoMethodError
 				raise "API didn't return any cinemas."
 			end
 			out[csymbol] = {}
 			# get list of EDIs played on particular date
-			films =  @c.films({ :cinema => cinema, :date => today })['films']
+			films =  Rails.cache.fetch("cached_films", :expires_in => seconds_to_midnight) do 
+				@c.films({ :cinema => cinema, :date => today })['films']
+			end
+
 			films.each do |film|
 				# from 'films' we need to resolve actual performances based on the EDI
 				edi = film['edi']
-				performances = @c.performances({ :cinema => cinema, :film => edi, :date => today})['performances']
+				performances = Rails.cache.fetch("cached_performances", :expires_in => seconds_to_midnight) do
+					@c.performances({ :cinema => cinema, :film => edi, :date => today})['performances']
+				end
 				performances.each do |perf|
 					london_time = DateTime.now.in_time_zone("Europe/London")
 					perfdate = DateTime.strptime(perf['time'], "%H:%M")
