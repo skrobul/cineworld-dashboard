dbdf115180eccdbffbf624432c029d81f1c6fa37
Marek Skrobacki <skrobul@skrobul.com>
2013-12-08 17:56:04 +0000

    add some logging

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -3,11 +3,13 @@ class NotificationMailer < ActionMailer::Base
 
   def new_films_email(films)
     @films = films
+    puts "Processing notifications for #{films.count} new films"
     recipients = [
         'alicja.kielbasa@gmail.com',
         'skrobul@skrobul.com',
     ]
     recipients.each do |recipient|
+        puts "Sending notification email to: #{recipient}"
         mail(to: recipient, subject: "Nowe filmy: #{Date.today}")
     end
   end
