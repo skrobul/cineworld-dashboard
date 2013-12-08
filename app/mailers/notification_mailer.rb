aef2ff20517a1ff82654d4edda0afda1d33ed2d6
Marek Skrobacki <skrobul@skrobul.com>
2013-12-08 18:08:29 +0000

    reconfigure mailer

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -8,9 +8,7 @@ class NotificationMailer < ActionMailer::Base
         'alicja.kielbasa@gmail.com',
         'skrobul@skrobul.com',
     ]
-    recipients.each do |recipient|
-        puts "Sending notification email to: #{recipient}"
-        mail(to: recipient, subject: "Nowe filmy: #{Date.today}")
-    end
+    puts "Sending notification email to: #{recipients}"
+    mail(to: recipients, subject: "Nowe filmy: #{Date.today}")
   end
 end
