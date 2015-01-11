21307cd88983a80745ea7e90bcd5ba900036685e
Marek Skrobacki <marek.skrobacki@rackspace.co.uk>
2015-01-11 18:26:47 +0000

    ignore non-english movies

--- app/models/film.rb
+++ app/models/film.rb
@@ -3,4 +3,12 @@ class Film < ActiveRecord::Base
   has_and_belongs_to_many :cinemas
   has_many :performances
   has_one :review
+  scope :name_exclude, ->(name) { where("title NOT LIKE ?", "%#{name}%")}
+  scope :notification_candidates, -> { where(notified: false)
+                                       .name_exclude('Hindi')
+                                       .name_exclude('Tamil')
+                                       .name_exclude('Punjab')
+                                       .name_exclude('Bollywood')
+  }
+
 end
